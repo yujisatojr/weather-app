96783b96def568b19a068d52e6b344e00f1e3954
Yuji Sato <yujisatojr@gmail.com>
2024-02-20 09:00:42 -0600

    Add static_folder path for flask

--- flask_server/app.py
+++ flask_server/app.py
@@ -1,6 +1,11 @@
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(
+    __name__,
+    static_url_path='',
+    static_folder='../client/build',
+    template_folder='../client/build'
+)
 
 from routes import *
 
